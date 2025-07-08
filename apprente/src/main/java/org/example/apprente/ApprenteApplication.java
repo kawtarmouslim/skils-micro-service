bc99aa3b0046dd31b973f78be0c6483eb2f855fe
kawtarmouslim <kawtharmouslim33@gmail.com>
2025-07-08 11:36:38 +0100

    structure

---
+++ apprente/src/main/java/org/example/apprente/ApprenteApplication.java
@@ -0,0 +1,13 @@
+package org.example.apprente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprenteApplication.class, args);
+    }
+
+}
---
+++ rendu/src/test/java/org/example/rendu/RenduApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rendu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
