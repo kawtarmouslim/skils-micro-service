c04ed15d07d866bef29f03f61c37f9169817a9e9
kawtarmouslim <kawtharmouslim33@gmail.com>
2025-07-10 12:53:13 +0100

    bref

--- bref/src/main/java/org/example/bref/service/BrefService.java
+++ bref/src/main/java/org/example/bref/service/BrefService.java
@@ -7,6 +7,9 @@ import org.example.bref.repository.BrefRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class BrefService {
@@ -16,6 +19,22 @@ public class BrefService {
         Bref bref = modelMapper.map(brefDto, Bref.class);
         Bref brefCreated = brefRepository.save(bref);
         return modelMapper.map(brefCreated, BrefDto.class);
+    }
+    public List<BrefDto> getAllBrefs() {
+        List<Bref> brefs = brefRepository.findAll();
+        return brefs.stream().map(bref -> modelMapper.map(bref, BrefDto.class)).collect(Collectors.toList());
+    }
+    public BrefDto UpdateBref( Long id,BrefDto brefDto) {
+        Bref bref = brefRepository.findById(id).orElse(null);
+        bref.setDebut(brefDto.getDebut());
+        bref.setTitre(brefDto.getTitre());
+        bref.setDescription(brefDto.getDescription());
+        bref.setFin(brefDto.getFin());
+        Bref brefUpdated = brefRepository.save(bref);
+        return modelMapper.map(brefUpdated, BrefDto.class);
+    }
+    public void deleteBref(Long id) {
+        brefRepository.deleteById(id);
     }
 
 }
--- competence/src/main/java/org/example/competence/repository/SousCompletenceRepository.java
+++ competence/src/main/java/org/example/competence/repository/SousCompletenceRepository.java
@@ -2,7 +2,10 @@ package org.example.competence.repository;
 
 import org.example.competence.entite.SousCompetence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SousCompletenceRepository extends JpaRepository<SousCompetence, Long> {
+
 
 }
